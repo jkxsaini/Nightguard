3b8fb2d34e52b68eb32f65d8cd35b0f76a92449c
Anton Riemer <anton.riemer@lebensbaum.local>
2026-05-31 17:31:08 +0200

    Antons branch

--- app/src/main/java/com/example/nightguard/MainActivity.kt
+++ app/src/main/java/com/example/nightguard/MainActivity.kt
@@ -1,47 +1,45 @@
 package com.example.nightguard
 
+
 import android.os.Bundle
+import androidx.compose.material3.*
+import androidx.compose.foundation.layout.*
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.nightguard.ui.theme.NightguardTheme
+import androidx.compose.ui.tooling.preview.Preview
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            NightguardTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            NightguardTheme(darkTheme = true, dynamicColor = false) {
+                    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                        MainScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
+@Preview(showSystemUi = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NightguardTheme {
-        Greeting("Android")
+fun MainScreenPreview() {
+    NightguardTheme(
+        darkTheme = true,
+        dynamicColor = false
+    ) {
+        Scaffold(
+            modifier = Modifier.fillMaxSize()
+        ) { innerPadding ->
+            MainScreen(
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
     }
 }
